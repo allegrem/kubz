1faed90bf5085d49729bfa36423a0e05a4e41532
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-12-11 17:16:19 +0100

    creating MainSynthesis class

---
+++ src/synthesis/MainSynthesis.java
@@ -0,0 +1,19 @@
+package synthesis;
+
+/**
+ * This main is used to test the synthesis engine. This should not be used
+ * in the final project.
+ * @author allegrem
+ */
+public class MainSynthesis {
+
+	/**
+	 * WARNING! This main should not be used in the final project!!
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
