e17a1c0ae4e7d2552b2ef20f80a8ef678833e077
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-12-13 22:33:27 +0100

    commenting OneInputBlock

--- src/synthesis/basicBlocks/OneInputBlock.java
+++ src/synthesis/basicBlocks/OneInputBlock.java
@@ -12,6 +12,13 @@ public abstract class OneInputBlock implements AudioBlock {
 
 	private AudioBlock in;
 	
+
+	/**
+	 * Plugs the given AudioBlock.
+	 * @param i This parameter is not used (gives 0 for example).
+	 * @throws TooManyInputsException if there is already an AudioBlock plugged
+	 * in
+	 */
 	@Override
 	public void plugin(AudioBlock a, int i) throws TooManyInputsException {
 		if (in == null)
@@ -20,30 +27,46 @@ public abstract class OneInputBlock implements AudioBlock {
 			throw new TooManyInputsException();
 	}
 
+	/**
+	 * Plugs out the given AudioBlock only if it is plugged in. If the plugged
+	 * AudioBlock is not the given one, this method does nothing.
+	 * @see OneInputBlock#plugout()
+	 */
 	@Override
 	public void plugout(AudioBlock a) {
 		if (in == a)
-			in = null;
+			plugout();
 	}
 
+	/**
+	 * Plugs out the plugged AudioBlock.
+	 * @deprecated The method without parameter should be used
+	 * @param i This parameter is not used.
+	 * @see OneInputBlock#plugout()
+	 */
 	@Override
 	public void plugout(int i) {
-		if (in == null)
-			System.out.println("Warning: tried to plug out an AudioBlock but " +
-					"no block was plugged in!");
-		in = null;
+		plugout();
 	}
 	
+	/**
+	 * Plugs out the plugged AudioBlock
+	 * If no AudioBlock is plugged in, a warning message is displayed.
+	 */
 	public void plugout() {
-		plugout(0);
-	}
-
-	@Override
-	public void plugoutAll() {
 		if (in == null)
 			System.out.println("Warning: tried to plug out an AudioBlock but " +
 					"no block was plugged in!");
 		in = null;
+	}
+
+	/**
+	 * This method is an alias for the plugout() method.
+	 * @see OneInputBlock#plugout()
+	 */
+	@Override
+	public void plugoutAll() {
+		plugout();
 	}
 	
 }
