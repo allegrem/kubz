b9d930ddc64fa266915efdd26f45c85d438d54d4
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2012-12-14 14:32:01 +0100

    Ajout de la classe Multiplier

---
+++ Audio/src/synthesis/MainSynthesis.java
@@ -0,0 +1,18 @@
+package synthesis;
+
+/**
+ * This main is used to test the synthesis engine. This should not be used
+ * in the final project.
+ * @author allegrem
+ */
+public class MainSynthesis {
+
+	/**
+	 * WARNING! This main should not be used in the final project!!
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
