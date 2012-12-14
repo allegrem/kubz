33b6e317678789a62895e8c83d319b5ba2fed78a
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2012-12-14 14:51:06 +0100

    Création de la classe Adder

---
+++ Audio/src/synthesis/Adder.java
@@ -0,0 +1,47 @@
+package synthesis;
+
+import java.util.ArrayList;
+
+/**
+ * Cette classe permet de faire la somme d’un nombre quelconque de signaux.
+ * @author valeh
+ */
+
+public class Adder implements AudioBlock {
+	
+	private ArrayList<AudioBlock> entries;
+	
+	public ArrayList<AudioBlock> getEntries(){
+		return entries;
+	}
+	public void setEntries( ArrayList<AudioBlock> entries ){
+		this.entries=entries;
+	}
+	
+	public Float play(int t) {
+		Float s=null;
+		for (AudioBlock a : entries){
+			s += a.play(t);
+		}
+		return s;
+	}
+	
+	public void plugin(AudioBlock a, int i) {
+		entries.add(a);	
+	}
+	
+	public void plugout(AudioBlock a) {
+		entries.remove(a);
+		
+	}
+	
+	public void plugout(int i) {
+		
+	}
+	
+	public void plugoutAll() {
+		entries=null;
+	}
+	
+	
+}
