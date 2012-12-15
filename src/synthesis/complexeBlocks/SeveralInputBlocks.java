9656f3e8dc7ea991489af2c75cb901d88e40469b
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2012-12-16 00:16:28 +0100

    Created SeveralBlocksInput

---
+++ src/synthesis/complexeBlocks/SeveralInputBlocks.java
@@ -0,0 +1,14 @@
+package synthesis.complexeBlocks;
+
+import synthesis.AudioBlock;
+
+/**
+ * This abstract class handles the plugin and plugout methods
+ * for AudioBlocks with several inputs.
+ * @author valeh
+ *
+ */
+public abstract class SeveralInputBlocks implements AudioBlock {
+	
+
+}
