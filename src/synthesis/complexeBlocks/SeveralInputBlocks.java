706b452ff2ccd06d3b4889f11ccb5c78bd0a00b6
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2012-12-16 00:19:06 +0100

    Added constructor in SeveralBlocksInput (SBI)

--- src/synthesis/complexeBlocks/SeveralInputBlocks.java
+++ src/synthesis/complexeBlocks/SeveralInputBlocks.java
@@ -1,6 +1,9 @@
 package synthesis.complexeBlocks;
 
+import java.util.ArrayList;
+
 import synthesis.AudioBlock;
+import synthesis.exceptions.RequireAudioBlocksException;
 
 /**
  * This abstract class handles the plugin and plugout methods
@@ -10,5 +13,33 @@ import synthesis.AudioBlock;
  */
 public abstract class SeveralInputBlocks implements AudioBlock {
 	
+	protected ArrayList<AudioBlock> entries;
+	
+	public SeveralInputBlocks(ArrayList<AudioBlock> entries){
+		this.entries = entries;
+	}
+	
+	public Float play(int t) {
+		if (entries == null) 
+			throw new RequireAudioBlocksException();
+		return null;
+	}
+	
+	public void plugin(AudioBlock a, int i) {
+		entries.add(a);	
+	}
+	
+	public void plugout(AudioBlock a) {
+		entries.remove(a);
+		
+	}
+	
+	public void plugout(int i) {
+		
+	}
+	
+	public void plugoutAll() {
+		entries=null;
+	}
 
 }
