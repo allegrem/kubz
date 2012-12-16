1d48a6f1066909c69c1a87ed54e9bc4b8161f49e
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-12-16 13:10:19 +0100

    fixing sound output + export in wav

--- src/synthesis/MainSynthesis.java
+++ src/synthesis/MainSynthesis.java
@@ -1,12 +1,19 @@
 package synthesis;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
 
-import synthesis.basicblocks.Adder;
 import synthesis.basicblocks.noinputblocks.SineWaveOscillator;
 import synthesis.basicblocks.noinputblocks.SquareWaveOscillator;
 import synthesis.basicblocks.oneinputblocks.Gain;
@@ -27,26 +34,35 @@ public class MainSynthesis {
 	 * @throws TooManyInputsException 
 	 * @throws RequireAudioBlocksException 
 	 * @throws LineUnavailableException 
+	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws TooManyInputsException, 
-	RequireAudioBlocksException, LineUnavailableException {
-		SineWaveOscillator osc = new SineWaveOscillator(110f, 120f);
-		SquareWaveOscillator osc2 = new SquareWaveOscillator(440f, 20f);
+	RequireAudioBlocksException, LineUnavailableException, IOException {
+		SineWaveOscillator osc = new SineWaveOscillator(440f, 120f);
+		SquareWaveOscillator osc2 = new SquareWaveOscillator(440f, 50f);
 		
-		Offset off = new Offset(60f);
-		off.plugin(osc);
+		AudioBlock out = osc2;
 		
-		Gain g = new Gain(2f);
-		g.plugin(osc);
 		
+		//playing sound
 		SourceDataLine line = initSoundSystem();
-		playSound(0f, 1f, osc, line);
-		playSound(0f, 2f, osc2, line);
+		playSound(0f, 1f, out, line);
 		closeSoundSystem(line);
 		
-/*		byte[] arr = computeSound(0f, 1f, osc2); 
-		for(int i = 0; i<1f*AudioBlock.sampleRate ; i++)
-			System.out.println(arr[i]);*/
+		
+		//save to wav
+		AudioFormat af = new AudioFormat(AudioBlock.sampleRate/2, 16, 1, true, true);
+				//i dont know why it plays the sound twice too fast, but it does :(
+        TargetDataLine lineOut = AudioSystem.getTargetDataLine(af);
+        lineOut.open(af);
+        lineOut.start();
+        
+        byte[] arr = computeSound(0f, 1f, out);
+        
+		AudioSystem.write(new AudioInputStream(new ByteArrayInputStream(arr), af, arr.length), 
+				AudioFileFormat.Type.WAVE, new File("fmout.wav"));
+		line.drain();
+        line.close();
 	}
 	
 	
@@ -73,7 +89,7 @@ public class MainSynthesis {
 	public static SourceDataLine initSoundSystem() 
 			throws LineUnavailableException {
 		final AudioFormat af = 
-				new AudioFormat(AudioBlock.sampleRate, 16, 1, true, true);
+				new AudioFormat(AudioBlock.sampleRate/2, 16, 1, true, true);
         SourceDataLine line = AudioSystem.getSourceDataLine(af);
         line.open(af);
         line.start();
