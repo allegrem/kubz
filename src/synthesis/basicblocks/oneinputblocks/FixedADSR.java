a07c482039d5d0e185968f6189819f23495cb4ed
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2012-12-25 17:56:00 +0100

    Completed the FixedADSR class

--- src/synthesis/basicblocks/oneinputblocks/FixedADSR.java
+++ src/synthesis/basicblocks/oneinputblocks/FixedADSR.java
@@ -1,43 +1,47 @@
 package synthesis.basicblocks.oneinputblocks;
 
+import synthesis.exceptions.RequireAudioBlocksException;
+
 public class FixedADSR extends OneInputBlock {
 	
-	/*private final float a,d,s,r;
-	private final float slevel;*/
+	private final float a,d,s,r;
+	private final float slevel, duration;
 	
 	
-	/*public FixedADSR(int a, int d, int s,int r, float slevel) throws nonValidproportionException, nonValidslevelException{  //check in adsr are a partition of duration
+	public FixedADSR(float a, float d, float s,float r, int duration, float slevel) /*throws nonValidproportionException, nonValidslevelException */{  //check in adsr are a partition of duration
 																								  //and also check if slevel<=in.getMax()			
 		this.slevel = slevel;
 		this.a = a;
 		this.d = d;
 		this.s = s;
 		this.r = r;
+		this.duration = duration;
 	}
-	public Float play(Float t) throws RequiredAudioBlockException {
+	
+	public Float play(Float t) throws RequireAudioBlocksException {
 		super.play(t);
-		float previous = in.play(t).floatValue();
-		float previousMax=in.getMax();
-		float tfloat = t.floatValue();
 		
+		float previous = in.play(t).floatValue();
+		float tfloat = t.floatValue();
 		float aDur=a*duration, dDur=aDur+d*duration,sDur=dDur+s*duration,rDur=sDur+r*duration;
 		
-		switch( tfloat ){
-		case (tfloat <= aDur ):
-			return new Float(previous*(previousMax/aDur)*tfloat);
-			break:
-		case (tfloat>a && tfloat<=dDur):
-			return new Float(previous*((slevel-previousMax)/d*duration)*tfloat);
-			break;
-		case (tfloat>dDur && tfloat<=sDur):
-			return new Float(previous*slevel);
-			break;
-		default:
-			return new Float(previous*(-slevel/(r*duration))*tfloat);
-			break;
-		}
+		float aExpr=(previous/aDur)*tfloat;
+		float dExpr = ( (slevel-previous)/(d*duration) )*(tfloat-dDur) + slevel;
+		float sExpr = slevel;
+		float rExpr = ( (-slevel)/r*duration )*(tfloat-sDur) + slevel;
+
+		if (tfloat <= aDur )
+			return new Float( previous*aExpr );
+		if (tfloat>aDur && tfloat<=dDur)
+			return new Float( previous*dExpr );
+		if (tfloat>dDur && tfloat<=sDur)
+			return new Float( previous*sExpr );
 		
-	}*/
+		return new Float( previous*rExpr );
+			
+		
+	
+	}
 	
 
 }
