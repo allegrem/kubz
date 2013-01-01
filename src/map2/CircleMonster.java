dca569929d2f33cca3e7336247e136ecb61a9760
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2013-01-01 22:23:14 +0100

    Déplcement de CircleMonster dans le package map2

---
+++ src/map2/CircleMonster.java
@@ -0,0 +1,13 @@
+/**
+ * Classe abstraite pour les monstres.
+ * @author:valeh
+ */
+package map2;
+
+import Map.Monster;
+
+public class CircleMonster extends Monster{
+	
+	public static int side;
+	
+}
