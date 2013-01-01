eadf106eca42e13d1cca93bf58c59178aa9f3522
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2013-01-01 22:46:36 +0100

    Ajout de commentaire

--- src/map2/tooFewArgumentsException.java
+++ src/map2/tooFewArgumentsException.java
@@ -1,3 +1,9 @@
+/**
+ * Exception leveée quand il n'y a pas les arguments nécessaires pour la création de Base et Wall lors 
+ * la lecture du fichier dans initFromFile.
+ * @author valeh 
+ */
+
 package map2;
 
 public class tooFewArgumentsException extends Exception{
