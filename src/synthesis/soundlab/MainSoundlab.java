cefea4074ea41942ac87aeaffdf53ef80d6f6140
Matthieu Allegre <matthieu.allegre@gmail.com>
2013-01-03 10:44:07 +0100

    creating SLWindow class

---
+++ src/synthesis/soundlab/MainSoundlab.java
@@ -0,0 +1,16 @@
+package synthesis.soundlab;
+
+/**
+ * @author allegrem
+ *
+ */
+public class MainSoundlab {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SLWindow slWindow = new SLWindow();
+	}
+
+}
---
+++ src/synthesis/soundlab/SLWindow.java
@@ -0,0 +1,27 @@
+package synthesis.soundlab;
+
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+/**
+ * @author allegrem
+ *
+ */
+public class SLWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @throws HeadlessException
+	 */
+	public SLWindow() {
+		super("kubz soundlab");
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setPreferredSize(new Dimension(600, 400));
+	    pack();
+	    setVisible(true);
+	}
+
+}
