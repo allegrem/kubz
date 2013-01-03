0ce3114c08a7c04da74e29bdc60da4ae3d6b604c
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2013-01-03 08:08:39 +0100

    Complété le corps du code de paint()

--- src/map2/CircleUnit.java
+++ src/map2/CircleUnit.java
@@ -1,6 +1,14 @@
+/**
+ * Unité Monstre de type cercle.
+ * @author valeh
+ */
+
 package map2;
 
 
+import static org.lwjgl.opengl.GL11.glColor3ub;
+
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.ReadableColor;
 
 public class CircleUnit extends Unit {
@@ -12,7 +20,14 @@ public class CircleUnit extends Unit {
 
 	@Override
 	public void paint() {
-		// TODO Auto-generated method stub
+		GL11.glBegin(GL11.GL_LINE_LOOP);
+		GL11.glColor3f(1.0f,0.0f,0.0f); 
+		
+		double xCenter = super.getX();
+		double yCenter = super.getY();
+		
+		GL11.glVertex3d(xCenter, yCenter, 0.0);
+		GL11.glEnd();
 
 	}
 
