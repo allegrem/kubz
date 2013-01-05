15618908fc0b4f9f92797035a5c62fff4c962393
paul <paul.berthier@outlook.com>
2013-01-05 11:05:47 +0100

    Modification de la classe Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,14 +7,12 @@ import OpenGL.GLBaseModule;
  * 
  */
 public class Main {
-	private static final int display_width=640;
-	private static final int display_length=480;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new MapCreator(display_width,display_length);
+		new MapCreator();
 	}
 
 }
