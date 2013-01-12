e672a620d658157d69750205bcacc90c8e047e80
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2013-01-12 16:57:28 +0100

    Created BellInstrument class

---
+++ src/synthesis/fmInstruments/BellInstrument.java
@@ -0,0 +1,32 @@
+package synthesis.fmInstruments;
+
+import java.util.ArrayList;
+
+import synthesis.exceptions.RequireAudioBlocksException;
+import synthesis.parameter.ParameterAudioBlock;
+
+public class BellInstrument implements FmInstrument{
+	//TODO (will come soon if my test works...)
+	
+	
+	
+	@Override
+	public Float play(Float t) throws RequireAudioBlocksException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Float phi(Float t) throws RequireAudioBlocksException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<ParameterAudioBlock> getParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
