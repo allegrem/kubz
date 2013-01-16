255312d3a89cc3ab1dbbc1015d0efc93ed2088c2
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2013-01-16 12:53:38 +0100

    création de la classe Distance

---
+++ src/utilities/Distance.java
@@ -0,0 +1,13 @@
+package utilities;
+
+public class Distance {
+	
+	public static double distanceToLine(Point A, Point B, Point P)
+	{
+	 double normalLength = Math.hypot(B.getX() - A.getX(), B.getY() - A.getY());
+	 return Math.abs((P.getX() - A.getX()) * (B.getY() - A.getY()) - (P.getY() - A.getY()) * (B.getX() - A.getX())) / normalLength;
+	}
+	
+	
+	
+}
