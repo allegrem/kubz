b170a4c80af4c023fd534e5bf052e244ee856ec2
paul <paul.berthier@outlook.com>
2013-01-22 15:41:52 +0100

    Creation des ArrayList dans le GameEngine

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import gameEngine.GameEngine;
 import map2.MapCreator;
 import OpenGL.GLDisplay;
 /**
@@ -13,8 +14,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new MapCreator(MapCreator.create_mode);
-		//new MapCreator(MapCreator.read_mode);
+		new GameEngine();
 	}
 
 }
