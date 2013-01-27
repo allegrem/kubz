350ef9579939722c6f5a5377fd7185fd5437e0ee
Felix <Felix@137.194.23.48>
2013-01-27 22:01:53 +0100

    début du moteur de jeu + commentaires su la classe Player

--- src/gameEngine/GameEngine.java
+++ src/gameEngine/GameEngine.java
@@ -1,19 +1,18 @@
 package gameEngine;
-import java.util.ArrayList;
 
-import monster.Monster;
+import monster.MonsterManager;
 import player.*;
-import cubeManager.*;
-import wall.*;
-import base.*;
 
+/**
+ * Pour l'instant c'est un test du moteur de jeu pour un seul joueur
+ * @author Felix
+ */
 
-public class GameEngine {
-	
-	private ArrayList<Monster> monsters;
-	private ArrayList<Wall> walls;
-	private ArrayList<Base> bases;
-	private CubeManager cubeManager;
-	
-
+public class GameEngine{
+	/**
+	 * pas de liste de joueurs car un seul joueur pour le moment,
+	 * de même une seule unité (Que 3 cubes pour le moment
+	 */
+	private MonsterManager monsterManager;
+	private Player player;
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,5 +1,10 @@
 package player;
 
+/**
+ * Classe qui représente un joueur, a des référence vers ses unités et paramètres
+ * @author Felix
+ */
+
 import parameter.*;
 import unit.*;
 
@@ -7,59 +12,85 @@ import unit.*;
 public abstract class Player {
 	
 	private Unit unit;
-	private Parameter[] parameters;
+	private Parameter[] parameters ;
 	
+	/**
+	 * Création d'un joueur avec une Unit et deux Parameter
+	 */
 	public Player(){
+		this.unit = new Unit();
+		this.parameters = new Parameter[2];
+		parameters[0]= new Parameter();
+		parameters[1]= new Parameter();
 		
 	}
 	
 	
-/*Bloc où on choisit l'état des paramettres*/
+	/**
+	 * Méthodes qui gèrent l'état des paramètres
+	 */
+	
 	public void setPStatesToFrozen(){
 		this.parameters[0].setToFrozen();
 		this.parameters[1].setToFrozen();
 	}
+	
 	public void setPStatesToSoundEdit(){
 		this.parameters[0].setToSoundEdit();
 		this.parameters[1].setToSoundEdit();
 	}
+	
 	public void setPStatesToWaiting(){
 		this.parameters[0].setToWaiting();
 		this.parameters[1].setToWaiting();
 	}
+	
 	public void setPStatesToPositionError(){
 		this.parameters[0].setToFrozen();
 		this.parameters[1].setToFrozen();
 	}
+	
+	/**
+	 * retourne un tableau avec les ParamState des different Parameters
+	 * @return paramsState
+	 */
 	public ParameterState[] getStates() {
-		/*  Bon là ça déconne un peu faut que je revois le type tableu
-		 * Parameter params[] = new Parameter[2](); 		
-		 *return {parameters[0].getState(),parameters[1].getState()};.*/
-		return null;
+		ParameterState[] paramsState = {parameters[0].getState(),parameters[1].getState()};
+		return paramsState;
 	}
 	
-/*Bloc où on choisit l'état de l'unité */	
+	/**
+	 * Bloc des méthodes où on choisit l'état de l'unité 
+	 */	
+	
 	public void setUStateToAngle(){
 		this.unit.setUStateToAngle();
 	}
+	
 	public void setUStateToDirection(){
 		this.unit.setUStateToDirection();
 	}	
+	
 	public void setUStateToFrozen(){
 		this.unit.setUStateToFrozen();
 	}
+	
 	public void setUStateToMoving(){
 		this.unit.setUStateToMoving();
 	}
+	
 	public void setUStateToPositionError(){
 		this.unit.setUStateToPositionError();
 	}
+	
 	public void setUStateToSelect(){
 		this.unit.setUStateToSelect();
-	}	
+	}
+	
 	public void setUStateToWaiting(){
 		this.unit.setUStateToWaiting();
-	}	
+	}
+	
 	public UnitState getState(){
 		return unit.getState();
 	}
