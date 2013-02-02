2f451d440add3b342c5b891e6b577053e44ccabb
Felix <Felix@HAL-9000>
2013-02-02 01:04:01 +0100

    mise en place de la vitesse des cubes pour l'interpolation

--- src/parameter/Parameter.java
+++ src/parameter/Parameter.java
@@ -7,6 +7,8 @@ public class Parameter {
 	
 	private Point pos;
 	private double angle;
+	private double xSpeed;
+	private double ySpeed;
 	private ParameterState state;
 	private CubeControlledView view;
 	
@@ -66,6 +68,19 @@ public class Parameter {
 	}
 	public double getAngle(){
 		return angle;
+	}
+	
+	public double getxSpeed() {
+		return xSpeed;
+	}
+	public void setxSpeed(double xSpeed) {
+		this.xSpeed = xSpeed;
+	}
+	public double getySpeed() {
+		return ySpeed;
+	}
+	public void setySpeed(double ySpeed) {
+		this.ySpeed = ySpeed;
 	}
 	
 	/**
--- src/unit/Unit.java
+++ src/unit/Unit.java
@@ -9,6 +9,8 @@ public class Unit {
 	
 	private double life;
 	private Point pos;
+	private double xSpeed;
+	private double ySpeed;
 	private double aperture;
 	private double direction;
 	private UnitState state;
@@ -118,6 +120,25 @@ public class Unit {
 		return direction;
 	}
 	
+	
+	
+	public double getxSpeed() {
+		return xSpeed;
+	}
+
+	public void setxSpeed(double xSpeed) {
+		this.xSpeed = xSpeed;
+	}
+
+	public double getySpeed() {
+		return ySpeed;
+	}
+
+	public void setySpeed(double ySpeed) {
+		this.ySpeed = ySpeed;
+	}
+
+
 	/**
 	 * setters et getter de UnitState
 	 */
