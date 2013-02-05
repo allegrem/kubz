f37ac1cacd628aa840d883624d93e8dd742b1fcc
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2013-02-06 00:37:56 +0100

    Writing applyFilter method and some complementary other methods...

--- src/synthesis/Sound.java
+++ src/synthesis/Sound.java
@@ -1,6 +1,7 @@
 package synthesis;
 
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -100,8 +101,54 @@ public class Sound extends Observable implements Observer {
 		updateSound();
 	}
 	
+	
 	protected void applyFilter(BandsFilter filter) {
-		//TODO
+		ArrayList<Double> soundFiltered = new ArrayList<Double>();
+		float time = 0;			
+		int sampleLength = (int) (AudioBlock.SAMPLE_RATE*20/1000); //*20ms sampling
+		int bandfreq = (int) (this.length*AudioBlock.SAMPLE_RATE/2)/11; //length of each band of the filter (11 bands on the whole)
+		for (int i=0;i<(int) (this.length*AudioBlock.SAMPLE_RATE/2)/sampleLength;i++){  //(length*SR/2) / sampleLength (n°of samples in the 
+			time += i*(20/1000);	//the i-th sampling														//first half
+			byte[] soundi = new byte[sampleLength];
+			for (int j=0;j<sampleLength;j++){
+				soundi[j] = sound[(int) (j+time*AudioBlock.SAMPLE_RATE)];
+			}			
+			Complex[] fourierCoeffs = computeFourier(soundi);
+			int bari = (int) ( (time+1)*AudioBlock.SAMPLE_RATE/bandfreq ); //which bar choose
+			int coeff = filter.getBar(bari);
+			double[] fourierFiltered = new double[fourierCoeffs.length];
+			for (int h = 0;h<fourierCoeffs.length;h++){						
+				fourierFiltered[i] = abs(fourierCoeffs[h]) * coeff;
+			}
+			Complex[] fourierInverse = computeFourierInverse(fourierFilterd);
+			for (int k=0;k<fourierInverse.length;k++){
+				double real = fourierFilterd[k].getReal();
+				soundFiltered.add(new Double(real));				
+			}
+															
+		}
+		
+	}
+	
+	
+	private Complex[] computeFourier(byte[] soundPiece) {
+		// looking for the smallest power of two above sound length
+		int power2Length = 1;
+		while (power2Length < soundPiece.length)
+			power2Length *= 2;
+
+		// converting byte array to double array
+		double[] doubleSoundPiece = new double[power2Length];
+		for (int i = 0; i < soundPiece.length; i++)
+			doubleSoundPiece[i] = soundPiece[i];
+		for (int i = soundPiece.length; i < power2Length; i++)
+			doubleSoundPiece[i] = 0; // add zeros at the end
+
+		// compute fourier transform (never ask me why it works, i dont know!!)
+		FastFourierTransformer fourier = new FastFourierTransformer(
+				DftNormalization.STANDARD);
+		return fourier.transform(doubleSoundPiece,
+				TransformType.FORWARD);
 	}
 	
 	public Sound filter(BandsFilter filter) {
