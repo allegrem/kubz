32605334ec9e43679e005ac2b390639812b8ccf4
Gu3nol - Guénolé LALLEMENT <guenole.lallement@gmail.com>
2013-02-06 09:45:05 +0100

    Modification de cube.java : ajout de commentaires et importation des bibliothèque XBee

--- src/cube/Cube.java
+++ src/cube/Cube.java
@@ -2,12 +2,25 @@ package cube;
 
 import utilities.Point;
 //import Xbee library
+import com.rapplogic.xbee.api.XBee;
+import com.rapplogic.xbee.api.XBeeAddress64;
+import com.rapplogic.xbee.api.XBeeException;
+import com.rapplogic.xbee.api.zigbee.ZNetTxRequest;
+import com.rapplogic.xbee.util.ByteUtils;
+
 
 public class Cube {	
+	/**
+	 * Attribute of cube
+	 */
 	private short angle;
 	private boolean tap;
 	private short id;
 	
+	/**
+	 * Method to set the RGB color of the LEDS
+	 * @parameters : chart of 
+	 */
 	public void setRGB(){
 		
 	}
