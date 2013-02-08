469e7deff8db4570c780875294014a14ef531412
paul <paul.berthier@outlook.com>
2013-02-08 20:02:07 +0100

    tata felix

--- src/monster/AttackType.java
+++ src/monster/AttackType.java
@@ -7,7 +7,7 @@ public abstract class AttackType {
 	public void attack(Unit unit){		
 	}
 	public ArrayList<int[]> result(){
-		System.out.println("tata");
+		System.out.println("tata felix");
 		return null;
 		
 	}
