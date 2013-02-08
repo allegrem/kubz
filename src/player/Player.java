05cb9e06c6d28d46653a968a8fae1b6535cc98c1
Felix Raimundo <felix.raimundo@telecom-paristech.fr>
2013-02-08 16:44:00 +0100

    Change choice

--- src/player/Player.java
+++ src/player/Player.java
@@ -201,12 +201,12 @@ public  class Player {
 		setPStatesToSoundEdit();
 		setUStateToWaiting();
 		while (!KeyboardManager.tap){
-				if(KeyboardManager.zKey) parameters[KeyboardManager.choice].translate(0,1);
-				if(KeyboardManager.sKey) parameters[KeyboardManager.choice].translate(0, -1);
-				if(KeyboardManager.qKey) parameters[KeyboardManager.choice].translate(-1, 0);
-				if(KeyboardManager.dKey) parameters[KeyboardManager.choice].translate(0, 1);
-				if(KeyboardManager.wKey) parameters[KeyboardManager.choice].rotate(1);
-				if(KeyboardManager.xKey) parameters[KeyboardManager.choice].rotate(-1);						
+				if(KeyboardManager.zKey) parameters[choice].translate(0,1);
+				if(KeyboardManager.sKey) parameters[choice].translate(0, -1);
+				if(KeyboardManager.qKey) parameters[choice].translate(-1, 0);
+				if(KeyboardManager.dKey) parameters[choice].translate(0, 1);
+				if(KeyboardManager.wKey) parameters[choice].rotate(1);
+				if(KeyboardManager.xKey) parameters[choice].rotate(-1);						
 		}
 		KeyboardManager.tap = false;
 	}	
