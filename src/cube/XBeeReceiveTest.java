d1e314749f82d9e94535fe20530ee52e30eb5dd9
daubresse <nicolas.daubresse@telecom-paristech.fr>
2013-02-08 17:08:48 +0100

    modification des class xbee

--- src/cube/XBeeReceiveTest.java
+++ src/cube/XBeeReceiveTest.java
@@ -1,9 +1,6 @@
 package cube;
 
-import com.rapplogic.xbee.api.XBeeException;
-
 //import Xbee library
-import cube.XBeeReceive;
 
 public class XBeeReceiveTest {
 
@@ -12,13 +9,8 @@ public class XBeeReceiveTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
-			XBeeReceive xbee = new XBeeReceive();
-		} catch (XBeeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+        XBeeReceive xbee = new XBeeReceive();
+
+    }
 
 }
