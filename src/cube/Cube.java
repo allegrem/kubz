e8c6c48df2eb612fb62aec64d51d3a7e8567c09c
daubresse <nicolas.daubresse@telecom-paristech.fr>
2013-02-09 13:39:15 +0100

    modifications des fonctions dans Cube.java

--- src/cube/Cube.java
+++ src/cube/Cube.java
@@ -15,30 +15,50 @@ public class Cube {
 
 	/**
 	 * Method to set the RGB color of the LEDS
-	 * @parameters : chart of 
+	 * @param : The 3 short that control R,G and B color.
 	 */
-	public void setRGB(){
+	public void setRGB(short R, short G, short B){
 		
 	}
-	
-	public void setIR() {
+
+    /**
+     * Method to switch on or off the IR LEDS using the pattern given
+     * @param : byte, the 3 last bit determined witch LEDS are switch on.
+     */
+	public void setIR(byte pattern){
+
+    }
+
+    /**
+     * Method to switch on the Motor using the pattern given
+     * @param : byte
+     */
+	public void setMotor(byte pattern) {
 		
 	}
-	
-	public void setMotor() {
-		
-	}
-	
+
+    /**
+     * Method to get the actual cube angle
+     * @return angle (float)
+     */
 	public float getAngle() {
 		return angle;
 	}
-	
+
+    /**
+     * Method to get the actual state of the tap
+     * @return tap (boolean)
+     */
 	public boolean getTap() {
 		return tap;
 	}
-	
+
+    /**
+     * Method to get the ID of the cube
+     * @return id (short)
+     */
 	public short getID() {
 		return id;
 
-}
+    }
 }
