45bb65fd57033b0084aff2bf9336c5ef00043337
Matthieu Allegre <matthieu.allegre@gmail.com>
2013-02-09 15:37:02 +0100

    comments

--- src/synthesis/Sound.java
+++ src/synthesis/Sound.java
@@ -14,8 +14,15 @@ import synthesis.filters.BandsFilter;
 import synthesis.fmInstruments.FmInstrument;
 
 /**
- * @author allegrem
+ * This class handles a sound and can perform some Fourier transforms and
+ * filtering. The sound is linked with a {@link FmInstrument}, and each time the
+ * FmInstrument is modified, the sound (and its spectrum) is updated. The length
+ * of the sound can also be edited. Finally the sound can be filtered with a
+ * {@link BandsFilter}, but in this case, if the FmInstrument changes again, the
+ * filtered sound will be erased. That's why a {@link Sound#lock()} method is
+ * provided to prevent the sound from being modified by instrument's updates.
  * 
+ * @author allegrem
  */
 public class Sound extends Observable implements Observer {
 
@@ -27,6 +34,8 @@ public class Sound extends Observable implements Observer {
 
 	private FmInstrument instrument;
 
+	private boolean locked = false; // block instrument observation
+
 	public Sound(FmInstrument instrument, float length) {
 		super();
 
@@ -35,6 +44,15 @@ public class Sound extends Observable implements Observer {
 		setLength(length);
 	}
 
+	/**
+	 * Change the length of the sound. The sound is updated after editing the
+	 * length.
+	 * 
+	 * @param length
+	 *            The length of the sound in seconds.
+	 * @throws InvalidParameterException
+	 *             if the length is negative
+	 */
 	public void setLength(float length) throws InvalidParameterException {
 		if (length < 0)
 			throw new InvalidParameterException("negative sound length");
@@ -42,6 +60,10 @@ public class Sound extends Observable implements Observer {
 		updateSound();
 	}
 
+	/**
+	 * Compute the sound using the instrument. At the end of the method,
+	 * {@link Sound#updateSpectrum()} is called.
+	 */
 	private void updateSound() {
 		try {
 			sound = new byte[(int) (length * AudioBlock.SAMPLE_RATE)];
@@ -55,6 +77,9 @@ public class Sound extends Observable implements Observer {
 		updateSpectrum();
 	}
 
+	/**
+	 * Compute the spectrum of the sound. It uses a FFT algorithm.
+	 */
 	private void updateSpectrum() {
 		// looking for the smallest power of two above sound length
 		int power2Length = 1;
@@ -68,46 +93,79 @@ public class Sound extends Observable implements Observer {
 		for (int i = sound.length; i < power2Length; i++)
 			originalSoundDouble[i] = 0; // add zeros at the end
 
-		// compute fourier transform (never ask me why it works, i dont know!!)
+		// compute fourier transform
 		FastFourierTransformer fourier = new FastFourierTransformer(
 				DftNormalization.STANDARD);
-		spectrum = fourier.transform(originalSoundDouble,
-				TransformType.FORWARD);
+		spectrum = fourier
+				.transform(originalSoundDouble, TransformType.FORWARD);
 	}
 
+	/**
+	 * Return the last computed sound (time domain).
+	 */
 	public byte[] getSound() {
 		return sound;
 	}
 
+	/**
+	 * Return the last computed spectrum of the sound (frequency domain).
+	 */
 	public Complex[] getSpectrum() {
 		return spectrum;
 	}
 
+	/**
+	 * Update the sound and its spectrum, unless the Sound is locked.
+	 * 
+	 * @see Sound#lock()
+	 * @see Sound#unlock()
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
-		updateSound();
+		if (!locked)
+			updateSound();
 
 		System.out.println("sound updated");
 	}
 
+	/**
+	 * Return the {@link FmInstrument} used to compute the sound.
+	 */
 	public FmInstrument getInstrument() {
 		return instrument;
 	}
 
+	/**
+	 * Modify the instrument linked with the sound and observe it. Then the
+	 * sound is updated.
+	 */
 	public void setInstrument(FmInstrument instrument2) {
 		this.instrument = instrument2;
 		instrument2.addObserver(this);
 		updateSound();
 	}
-	
+
 	protected void applyFilter(BandsFilter filter) {
-		//TODO
+		// TODO
 	}
-	
+
 	public Sound filter(BandsFilter filter) {
 		Sound filteredSound = new Sound(instrument, length);
 		filteredSound.applyFilter(filter);
 		return filteredSound;
+	}
+
+	/**
+	 * Lock the sound so that it is not updated if the instrument is modified.
+	 */
+	public void lock() {
+		locked = true;
+	}
+	/**
+	 * Unlock the sound so that it is updated if the instrument is modified.
+	 */
+	public void unlock() {
+		locked = false;
 	}
 
 }
