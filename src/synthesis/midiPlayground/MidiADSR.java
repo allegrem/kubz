bcb6b3efbd1ffb7a3227733f4cd5c425897d8ce6
Matthieu Allegre <matthieu.allegre@gmail.com>
2013-02-25 18:52:42 +0100

    start to comment midi code

--- src/synthesis/midiPlayground/MidiADSR.java
+++ src/synthesis/midiPlayground/MidiADSR.java
@@ -1,25 +1,42 @@
 package synthesis.midiPlayground;
 
+/**
+ * This class handles an ADSR envelope. The max output level is given by the
+ * velocity of the {@link MidiCommand}. The envelope responds to the MIDI
+ * command : it starts when a {@link MidiCommand#NOTE_ON} command is received,
+ * and the release starts when a {@link MidiCommand#NOTE_OFF} command is
+ * received. The four parameters of the envelope (attack, decay, sustain,
+ * release) are controlled by {@link MidiAudioBlock}, but it is highly
+ * recommended to plug a {@link MidiConstant} or a {@link MidiParam} in (since
+ * their value won't change).
+ * 
+ * @author allegrem
+ */
 public class MidiADSR implements MidiAudioBlock {
 
-	private final MidiAudioBlock a;
-	private final MidiAudioBlock d;
-	private final MidiAudioBlock s;
-	private final MidiAudioBlock r;
-	private float amplitude;
-	private float releaseTime = 0;
-	private boolean releasing = false;
-	private float releaseAmp = 0;
+	private final MidiAudioBlock a; // attack
+	private final MidiAudioBlock d; // decay
+	private final MidiAudioBlock s; // sustain
+	private final MidiAudioBlock r; // release
+
+	private float amplitude; // amplitude given by the NOTE_ON command
+	private float releaseTime = 0; // time when the release started
+	private boolean releasing = false; // has the release started?
+	private float releaseAmp = 0; // current amplitude when the release started
 
 	/**
+	 * Create a new MidiADSR object. The four parameters should be
+	 * {@link MidiConstant} or {@link MidiParam} (since their value won't
+	 * change).
+	 * 
 	 * @param a
-	 *            time in seconds
+	 *            attack time in seconds
 	 * @param d
-	 *            time in seconds
+	 *            decay time in seconds
 	 * @param s
-	 *            between 0 and 1
+	 *            sustain level between 0 and 1
 	 * @param r
-	 *            time in seconds
+	 *            release time in seconds
 	 */
 	public MidiADSR(MidiAudioBlock a, MidiAudioBlock d, MidiAudioBlock s,
 			MidiAudioBlock r) {
@@ -30,48 +47,61 @@ public class MidiADSR implements MidiAudioBlock {
 		this.r = r;
 	}
 
-	//return a value between 0 and amplitude
+	/**
+	 * Return the value of the envelope according to the given time. The output
+	 * is between 0 and the amplitude (given in the MIDI command). When the
+	 * release phase is finished, return 0.
+	 */
 	@Override
 	public Float play(Float t) {
 		float a_ = a.play(t), s_ = s.play(t), d_ = d.play(t), r_ = r.play(t);
 		float result;
-		
+
+		// here the max output level is 1
 		if (releasing) {
-			if (t < releaseTime + r_)
-				result = - releaseAmp / r_ * t + releaseAmp + releaseAmp * releaseTime / r_;
+			if (t < releaseTime + r_) // release phase
+				result = -releaseAmp / r_ * t + releaseAmp + releaseAmp
+						* releaseTime / r_;
 			else
+				// after release phase
 				result = 0;
-		}
-		else if (t < a_ && a_ > 0) {
+		} else if (t < a_ && a_ > 0) { // attack phase
 			result = t / a_;
-		} else if (t < a_ + d_  &&  d_ > 0) {
+		} else if (t < a_ + d_ && d_ > 0) { // decay phase
 			result = t * (s_ - 1f) / d_ + 1f + (1f - s_) / d_ * a_;
-		} else {
+		} else { // sustain phase
 			result = s_;
 		}
-		
+
+		// save the amplitude and the time until the release phase starts
 		if (!releasing) {
 			releaseAmp = result;
 			releaseTime = t;
 		}
-		
+
+		// now the max output level is amplitude
 		result *= amplitude;
 		return result;
 	}
 
 	@Override
 	public Float phi(Float t) {
-		// TODO Auto-generated method stub
+		// FIXME not implemented
+		System.out.println("ERROR! phi function not implemented in MidiADSR!!");
 		return null;
 	}
 
+	/**
+	 * When a {@link MidiCommand#NOTE_ON} command is received, update the
+	 * amplitude with the given amplitude. When a {@link MidiCommand#NOTE_OFF}
+	 * command is received, start the release phase.
+	 */
 	@Override
 	public void command(MidiCommand command) {
 		if (command.getCommand() == MidiCommand.NOTE_ON) {
-			amplitude = (float)command.getParam2();
+			amplitude = (float) command.getParam2();
 			releasing = false;
-		}
-		else if (command.getCommand() == MidiCommand.NOTE_OFF)
+		} else if (command.getCommand() == MidiCommand.NOTE_OFF)
 			releasing = true;
 	}
 
--- src/synthesis/midiPlayground/MidiCommand.java
+++ src/synthesis/midiPlayground/MidiCommand.java
@@ -1,20 +1,41 @@
 package synthesis.midiPlayground;
 
+/**
+ * This class embodies a MIDI command, with a command code and two parameters.
+ * You should take a look at the constants defined in this class, which
+ * represents the available MIDI command with a description of the two
+ * associated parameters.
+ * 
+ * @author allegrem
+ */
 public class MidiCommand {
-	
+
+	/**
+	 * Fired when a key is pressed. Param1: note code. Param2: velocity.
+	 */
 	public final static int NOTE_ON = 144;
+
+	/**
+	 * Fired when a key is released. Param1: note code. Param2: velocity.
+	 */
 	public final static int NOTE_OFF = 128;
 
 	private final int command;
-	
+
 	private final int param1;
-	
+
 	private final int param2;
 
 	/**
+	 * Create a new MidiCommand.
+	 * 
 	 * @param command
+	 *            See the constants defined in {@link MidiCommand} to know which
+	 *            commands are available.
 	 * @param param1
+	 *            See the command description to know which parameter it is.
 	 * @param param2
+	 *            See the command description to know which parameter it is.
 	 */
 	public MidiCommand(int command, int param1, int param2) {
 		super();
@@ -31,19 +52,19 @@ public class MidiCommand {
 	}
 
 	/**
-	 * @return the param1
+	 * @return the param1 (see each command for more informations about this
+	 *         parameter)
 	 */
 	public int getParam1() {
 		return param1;
 	}
 
 	/**
-	 * @return the param2
+	 * @return the param2 (see each command for more informations about this
+	 *         parameter)
 	 */
 	public int getParam2() {
 		return param2;
 	}
-	
-	
-	
+
 }
