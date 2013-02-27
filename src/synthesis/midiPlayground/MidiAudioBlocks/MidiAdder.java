ad40bf21cf0375011e118d8ee664a0701969d147
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2013-02-27 21:43:48 +0100

    MidiAdder class

---
+++ src/synthesis/midiPlayground/MidiAudioBlocks/MidiAdder.java
@@ -0,0 +1,33 @@
+package synthesis.midiPlayground.MidiAudioBlocks;
+
+import synthesis.midiPlayground.MidiCommand;
+
+public class MidiAdder implements MidiAudioBlock{
+	
+	private MidiAudioBlock block1;
+	private MidiAudioBlock block2;
+	
+	
+	public MidiAdder(MidiAudioBlock block1, MidiAudioBlock block2){
+		this.block1 = block1;
+		this.block2 = block2;		
+	}
+	@Override
+	public Float play(Float t) {		
+		return block1.play(t).floatValue() + block2.play(t).floatValue() ;
+	}
+
+	@Override
+	public Float phi(Float t) {
+		return block1.phi(t).floatValue() + block2.phi(t).floatValue() ;
+	}
+
+	@Override
+	public void command(MidiCommand command) {
+		block1.command(command);
+		block2.command(command);
+		
+	}
+	
+
+}
