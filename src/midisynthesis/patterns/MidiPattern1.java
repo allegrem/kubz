da30b5cf0e39accc63e2f1e8f8cf1352d6c378f3
valeh <valeh.valiollah-pour-amiri@telecom-paristech.fr>
2013-03-24 14:24:10 +0100

    fixed problems in patterns+added XylophoneInstr

--- src/midisynthesis/patterns/MidiPattern1.java
+++ src/midisynthesis/patterns/MidiPattern1.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import midisynthesis.midicommand.DelayedMidiCommand;
 import midisynthesis.midicommand.MidiCommand;
 
-
-
+/**
+ * 
+ * @author valeh
+ *
+ */
+//starting tune : 65 (F4)
 
 public class MidiPattern1 extends MidiPattern{
 	
@@ -19,7 +23,7 @@ public class MidiPattern1 extends MidiPattern{
 	@Override
 	protected void buildPattern(){
 		pattern.add(new DelayedMidiCommand(new MidiCommand(MidiCommand.NOTE_ON, 0, 100), 1f));
-		pattern.add(new DelayedMidiCommand(new MidiCommand(MidiCommand.NOTE_OFF, 0, 100), 1.5f));
+		pattern.add(new DelayedMidiCommand(new MidiCommand(MidiCommand.NOTE_OFF, 0, 100), 1.5f));			
 		pattern.add(new DelayedMidiCommand(new MidiCommand(MidiCommand.NOTE_ON, 4, 100), 2f));
 		pattern.add(new DelayedMidiCommand(new MidiCommand(MidiCommand.NOTE_OFF, 4, 100), 2.5f));
 		pattern.add(new DelayedMidiCommand(new MidiCommand(MidiCommand.NOTE_ON, 7, 100), 3f));
